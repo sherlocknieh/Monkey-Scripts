99fea1a8e117a6cd202fcba72b2823ab12fdd8ef
sherlocknieh <sherlocknieh@gmail.com>
2025-11-06 23:29:15 +0800

    更新

--- Debug-Script.user.js
+++ Debug-Script.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://**/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description 2025/11/5 14:00:22
 // ==/UserScript==
